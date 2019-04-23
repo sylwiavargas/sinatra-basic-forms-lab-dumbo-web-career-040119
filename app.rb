6f8611fef620a3b3815fb0470b0f8a64f7f6d118
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-04-23 12:04:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get ('/') {
+    erb :index
+  }
+
+  get ('/new') {
+    erb :create_puppy
+  }
+
+  post ('/puppy') {
+    @name = params[:name]
+    @breed = params[:breed]
+    @months_old = params[:months_old]
+    erb :display_puppy
+  }
+  
 end
